096811b0a8587b0843fa3d1c1f62dc98827a857a
samin Sathik <saminsathik31@gmail.com>
2026-07-27 21:14:08 +0530

    Delete chapter3/LOOPS directory

--- chapter3/LOOPS/Table.java
+++
@@ -1,9 +0,0 @@
-public class Table {
-    public static void main(String[] args) {
-
-        for(int i = 1; i <= 10; i++) {
-            System.out.println("5 x " + i + " = " + (5 * i));
-        }
-
-    }
-}
